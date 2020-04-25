3509b3c1e3eb6bf6e186d077744cb41a3b71aba3
hyx546 <love.1179612384@qq.com>
2020-04-25 20:06:24 +0800

    手动实现一下instanceof的功能

---
+++ 源码/手写instanceof.js
@@ -0,0 +1,14 @@
+function myInstanceof (left ,right) {
+  if(typeof left !== 'object' || left === null) return false;
+  //  Object.getPrototypeOf 可以拿到参数的原型对象
+  let proto = Object.getPrototypeOf(left)
+  while (true) {
+    if (proto === null) return false;
+    if(proto === right.prototype) return true;
+    proto = Object.getPrototypeOf(proto)
+  }
+}
+console.log('====================================');
+console.log(myInstanceof('111',String));
+console.log('====================================');
+console.log(myInstanceof(new String('111'),String));
