85a53cd2d604635ee0533e1dd74ff40a4c97eb9b
hyx546 <love.1179612384@qq.com>
2020-04-27 13:40:17 +0800

    用defineProperty实现一个私有变量，可以通过方法访问，但不能直接访问

--- 源码/defineProperty.js
+++ 源码/defineProperty.js
@@ -1,16 +1,17 @@
 // 如何实现一个私有变量，可以通过getName方法可以访问，不能直接访问
+// defineProperty  直接在对象上添加一个新的属性，或修改对象上的现有属性
 
-// var obj = {
-//   name: 'hanyuxi',
-//   getName:function() {
-//     return this.name
-//   }
-// }
-// Object.defineProperty(obj,name,{
+var obj = {
+  name: 'hanyuxi',
+  getName:function() {
+    return this.name
+  }
+}
+Object.defineProperty(obj,name,{
   
-// })
+})
 
-// console.log();
+console.log();
 
 console.log(NaN===NaN);
 
