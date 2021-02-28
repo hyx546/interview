03f1fbdfa4d52f18068b54ac2a46b2ee30cb0316
hyx546 <love.1179612384@qq.com>
2021-02-28 15:17:37 +0800

    async中await表达式为promise时的状态处置

--- js/promise/async.js
+++ js/promise/async.js
@@ -1,11 +1,19 @@
 function havePromise() {
   console.log(1);
-  return 100
+  return new Promise((resolve, reject) => {
+    resolve(100)
+  })
 }
 
 async function getResult() {
-  let a = await havePromise();
-  console.log(a);
+  try {
+    // 表达式如果为promise，必须等到resolve才会执行下面的代码
+    // 如果为reject，通过catch可以捕捉到reason
+    let a = await havePromise();
+    console.log(a);
+  } catch (error) {
+    console.log(error);
+  }
 }
 getResult();
 console.log(3);
