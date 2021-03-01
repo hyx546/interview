57c9f7695ac8b9419b86f84180b1bbb4d870271e
hyx546 <love.1179612384@qq.com>
2021-03-01 22:22:16 +0800

    [深拷贝]：实现

--- 源码/deepClone.js
+++ 源码/deepClone.js
@@ -1,9 +1,13 @@
 function deepClone(obj) {
-  let newArr = obj instanceof Array ? []:{}
-  for(let i in obj) {
-    newArr[i] =typeof obj[i] == 'object'?
-    deepClone(obj[i]) :obj[i]
+  // 非引用类型，直接返回
+  if (!obj || typeof obj !== 'object') return obj
+  //创建一个变量用来存储对象或者数组类型
+  let newArr = obj instanceof Array ? [] : {};
+  for(let i in obj){
+    newArr[i] = typeof obj[i]  === 'object' ? deepClone(obj[i]) : obj[i]
   }
-  return newArr
+  return newArr 
 }
+
+console.log(deepClone([1,2,3,4,5]));
 
