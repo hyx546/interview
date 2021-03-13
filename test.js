51fab7dd272294c8c5bd8dc30354b079a3d1aa07
hyx546 <love.1179612384@qq.com>
2021-03-13 18:46:27 +0800

    响应式布局，高度始终为宽度的50%

---
+++ test.js
@@ -0,0 +1,6 @@
+if (1 == '1') { console.log('1') }
+if (1 === '1') { console.log('2') }
+if ([1] == [1]) { console.log('3') }
+if ([1] === [1]) { console.log('4') }
+if (0.1 + 0.2 == 0.3) { console.log('5') }
+if (0.1 + 0.2 === 0.3) { console.log('6') }
