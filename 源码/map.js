db4d41dc648b933b7f7286b94a6f27dfe0672a75
hyx546 <love.1179612384@qq.com>
2021-03-15 12:10:54 +0800

    [map实现]

--- 源码/map.js
+++ 源码/map.js
@@ -4,8 +4,13 @@ Array.prototype.myMap = function (fn, context) {
   const self = this;
 
   const ctx = context ? context : self;
+
+  if (typeof fn !== 'function') {
+    throw new Error(`${fn} is not a function`)
+  }
+
   for (let i = 0; i < self.length; i++) {
-    arr.push(fn.call(ctx,self[i],i,self))
+    arr.push(fn.call(ctx, self[i], i, self))
   }
 
   return arr
