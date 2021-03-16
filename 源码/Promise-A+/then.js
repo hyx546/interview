848a989768e0a1754e75c7e06e6254c8e3c87e6e
hyx546 <love.1179612384@qq.com>
2021-03-16 21:32:18 +0800

    [promise.then]:实现b请求的参数是a请求的返回值

--- 源码/Promise-A+/then.js
+++ 源码/Promise-A+/then.js
@@ -1,21 +1,34 @@
-myPromise.prototype.then = (onFulfilled, onRejected) => {
+// myPromise.prototype.then = (onFulfilled, onRejected) => {
 
-  const self = this;
-  onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
-  onRejected = typeof onRejected === 'function' ? onRejected : reason => { throw (reason) }
-  if (self.status === 'fulfilled') {
-    return new Promise((resolve, reject) => {
+//   const self = this;
+//   onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : value => value;
+//   onRejected = typeof onRejected === 'function' ? onRejected : reason => { throw (reason) }
+//   if (self.status === 'fulfilled') {
+//     return new Promise((resolve, reject) => {
 
-    })
-  }
-  if (self.status === 'rejected') {
-    return new Promise((resolve, reject) => {
+//     })
+//   }
+//   if (self.status === 'rejected') {
+//     return new Promise((resolve, reject) => {
 
-    })
-  }
-  if (self.status === 'pending') {
-    return new Promise((resolve, reject) => {
+//     })
+//   }
+//   if (self.status === 'pending') {
+//     return new Promise((resolve, reject) => {
 
-    })
-  }
+//     })
+//   }
+// }
+
+
+function test(a, b) {
+  return new Promise((resolve, reject) => {
+    console.log('------1',a);
+    resolve(1)
+  }).then((id) => {
+    console.log(id);
+    console.log('-----2',b);
+  })
 }
+
+console.log(test(1,2));
