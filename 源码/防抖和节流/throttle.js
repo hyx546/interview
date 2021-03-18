31c36da66220f37322b1f8b6032bd7aa62ee2dac
hyx546 <love.1179612384@qq.com>
2021-03-18 22:53:13 +0800

    [节流]:时间戳和定时器的实现

--- 源码/防抖和节流/throttle.js
+++ 源码/防抖和节流/throttle.js
@@ -1,6 +1,9 @@
-// 当触发事件时，没有设置定时器就设置一个
-// 当定时器中被执行后，把定时器设置为null
-
+/**
+ * 使用定时器的话不会立即执行，停止触发后还会执行一次
+ * @param {*} fn 
+ * @param {*} delay 
+ * @returns 
+ */
 function throttle(fn, delay) {
   let timeout;
   return function () {
@@ -10,6 +13,24 @@ function throttle(fn, delay) {
         timeout = null
         fn.apply(self, arguments);
       }, delay)
+    }
+  }
+}
+
+/**
+ * 使用时间戳会立即执行，停止后会立即执行
+ */
+
+function throttle(fn,delay){
+  // 对比时间戳，初始化为0则会立即执行，初始化为当前时间戳贼delay秒后执行
+  let previous = 0;
+  return function(){
+    const self = this;
+    let now = new Date().getTime();
+    // 间隔大于wait则执行并更新对比时间戳
+    if(now - previous > delay){
+      fn.apply(self,arguments);
+      previous = now;
     }
   }
 }
